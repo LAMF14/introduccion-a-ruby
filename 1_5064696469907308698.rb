fd036e963670e37039cfc65ddc56ef55848bbc20
LAMF14 <65782418+LAMF14@users.noreply.github.com>
2020-05-22 22:13:56 -0600

    Add files via upload

---
+++ 1_5064696469907308698.rb
@@ -0,0 +1,15 @@
+
+#puts "hola mundo"
+
+#nombre="Eduin"
+#puts nombre
+#nombre=10
+#puts nombre
+
+#nombre_del_tutor="Eduin Zelaya"
+#puts nombre_del_tutor
+
+puts "Final del video"
+despedida=gets
+puts "Final del video #{despedida}"
+
